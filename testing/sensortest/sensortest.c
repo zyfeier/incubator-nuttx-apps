77c62eb388522a41bb9cf3cd6b6719be053f659a
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-11-17 23:37:41 +0800

    testing/sensortest: Fix printf format warning
    sensortest.c: In function 'print_gps':
    Error: sensortest.c:169:29: error: format '%llu' expects argument of type 'long long unsigned int', but argument 3 has type 'uint64_t' {aka 'long unsigned int'} [-Werror=format=]
      169 |   printf("%s: timestamp: %llu time_utc: %llu latitude: %f longitude: %f "
          |                          ~~~^
          |                             |
          |                             long long unsigned int
          |                          %lu
    ......
      172 |          " %u\n", name, event->timestamp, event->time_utc, event->latitude,
          |                         ~~~~~~~~~~~~~~~~
          |                              |
          |                              uint64_t {aka long unsigned int}
    Error: sensortest.c:169:44: error: format '%llu' expects argument of type 'long long unsigned int', but argument 4 has type 'uint64_t' {aka 'long unsigned int'} [-Werror=format=]
      169 |   printf("%s: timestamp: %llu time_utc: %llu latitude: %f longitude: %f "
          |                                         ~~~^
          |                                            |
          |                                            long long unsigned int
          |                                         %lu
    ......
      172 |          " %u\n", name, event->timestamp, event->time_utc, event->latitude,
          |                                           ~~~~~~~~~~~~~~~
          |                                                |
          |                                                uint64_t {aka long unsigned int}
    sensortest.c: In function 'print_gps_satellite':
    Error: sensortest.c:183:29: error: format '%llu' expects argument of type 'long long unsigned int', but argument 3 has type 'uint64_t' {aka 'long unsigned int'} [-Werror=format=]
      183 |   printf("%s: timestamp: %llu count: %u satellites: %u", name,
          |                          ~~~^
          |                             |
          |                             long long unsigned int
          |                          %lu
      184 |          event->timestamp, event->count, event->satellites);
          |          ~~~~~~~~~~~~~~~~
          |               |
          |               uint64_t {aka long unsigned int}
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
    Change-Id: I50a0de111813b234b05da3a8c100b824942645fc

--- testing/sensortest/sensortest.c
+++ testing/sensortest/sensortest.c
@@ -164,10 +164,10 @@ static void print_gps(const char *buffer, const char *name)
 {
   struct sensor_event_gps *event = (struct sensor_event_gps *)buffer;
 
-  printf("%s: timestamp: %llu time_utc: %llu latitude: %f longitude: %f "
-         "altitude: %f altitude_ellipsoid: %f eph: %f epv: %f "
-         "hdop: %f vdop: %f ground_speed: %f course: %f satellites_used: %u\n",
-         name, event->timestamp, event->time_utc, event->latitude,
+  printf("%s: timestamp:%" PRIu64 " time_utc: %" PRIu64 " latitude: %f "
+         "longitude: %f altitude: %f altitude_ellipsoid: %f eph: %f epv: %f "
+         "hdop: %f vdop: %f ground_speed: %f course: %f satellites_used:"
+         " %u\n", name, event->timestamp, event->time_utc, event->latitude,
          event->longitude, event->altitude, event->altitude_ellipsoid,
          event->eph, event->epv, event->hdop, event->vdop,
          event->ground_speed, event->course, event->satellites_used);
@@ -178,7 +178,7 @@ static void print_gps_satellite(FAR const char *buffer, FAR const char *name)
   FAR struct sensor_event_gps_satellite *event =
         (struct sensor_event_gps_satellite *)buffer;
 
-  printf("%s: timestamp: %llu count: %u satellites: %u", name,
+  printf("%s: timestamp: %" PRIu64 " count: %u satellites: %u", name,
          event->timestamp, event->count, event->satellites);
 }
 
