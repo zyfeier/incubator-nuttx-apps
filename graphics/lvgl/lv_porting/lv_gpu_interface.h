3ec7b652673e87087535c8ed4f1699a16ecab532
Peter Bee <bijunda1@xiaomi.com>
2021-12-13 15:33:24 +0800

    apps/lv_gpu_interface: update to match latest lvgl
    CHAMPION-529
    
    Signed-off-by: Peter Bee <bijunda1@xiaomi.com>
    Change-Id: I6cd8cb9693d1872fabcbac8a4d4aa71cd2225f01

--- graphics/lvgl/lv_porting/lv_gpu_interface.h
+++ graphics/lvgl/lv_porting/lv_gpu_interface.h
@@ -117,49 +117,28 @@ lv_gpu_mode_t lv_gpu_getmode(void);
 lv_res_t lv_gpu_setmode(lv_gpu_mode_t mode);
 
 /****************************************************************************
- * Name: _lv_fill_gpu
+ * Name: lv_draw_map_gpu
  *
  * Description:
- *   Use GPU to fill rectangular area in buffer.
+ *   Copy a transformed map (image) to a display buffer.
  *
  * Input Parameters:
- *   @param[in] dest_buf Destination buffer pointer (must be aligned on 32 bytes)
- *   @param[in] dest_width Destination buffer width in pixels (must be aligned on 16 px)
- *   @param[in] dest_height Destination buffer height in pixels
- *   @param[in] fill_area Area to be filled
- *   @param[in] color Fill color
- *   @param[in] opa Opacity (255 = full, 128 = 50% background/50% color, 0 = no fill)
+ * @param map_area area of the image  (absolute coordinates)
+ * @param clip_area clip the map to this area (absolute coordinates)
+ * @param map_buf a pixels of the map (image)
+ * @param opa overall opacity in 0x00..0xff range
+ * @param chroma chroma key color
+ * @param angle rotation angle (= degree*10)
+ * @param zoom image scale in 0..65535 range, where 256 is 1.0x scale
+ * @param mode blend mode from `lv_blend_mode_t`
  *
  * Returned Value:
- *   @retval LV_RES_OK Fill completed
- *   @retval LV_RES_INV Error occurred
+ *   None
  *
  ****************************************************************************/
 
-LV_ATTRIBUTE_FAST_MEM lv_res_t _lv_fill_gpu(lv_color_t* dest_buf, lv_coord_t dest_width, lv_coord_t dest_height,
-    const lv_area_t* fill_area, lv_color_t color, lv_opa_t opa);
-
-/****************************************************************************
- * Name: _lv_map_gpu
- *
- * Description:
- *   Use GPU to draw a picture, optionally transformed.
- *
- * Input Parameters:
- *   @param[in] clip_area Clip output to this area
- *   @param[in] map_area The location of image BEFORE transformation
- *   @param[in] map_buf Source image buffer address
- *   @param[in] lv_draw_img_dsc_t Draw image descriptor
- *   @param[in] alpha_byte Alpha-byte color format, such as BGRA5658
- *
- * Returned Value:
- *   @retval LV_RES_OK Blit completed
- *   @retval LV_RES_INV Error occurred
- *
- ****************************************************************************/
-
-LV_ATTRIBUTE_FAST_MEM lv_res_t _lv_map_gpu(const lv_area_t* clip_area, const lv_area_t* map_area,
-    const lv_color_t* map_buf, const lv_draw_img_dsc_t* draw_dsc, bool alpha_byte);
+LV_ATTRIBUTE_FAST_MEM lv_res_t lv_draw_map_gpu(const lv_area_t* map_area, const lv_area_t* clip_area,
+    const lv_color_t* map_buf, const lv_draw_img_dsc_t* draw_dsc, bool chroma_key, bool alpha_byte);
 
 #undef EXTERN
 #ifdef __cplusplus
