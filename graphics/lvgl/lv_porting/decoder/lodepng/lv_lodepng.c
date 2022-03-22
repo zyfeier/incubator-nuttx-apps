03bcdf89ba6ca2dd7dd79aeeb58e3c990e2567b9
pengyiqiang <pengyiqiang@xiaomi.com>
2022-03-22 15:55:49 +0800

    lvgl: fix lodepng warning
    CHAMPION-000
    
    Change-Id: I5483a22ec571a5af9b1060a614e2252ca85abc40
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- graphics/lvgl/lv_porting/decoder/lodepng/lv_lodepng.c
+++ graphics/lvgl/lv_porting/decoder/lodepng/lv_lodepng.c
@@ -125,7 +125,7 @@ static lv_res_t decoder_open(lv_img_decoder_t * decoder, lv_img_decoder_dsc_t * 
 
         uint32_t error = lodepng_load_file(&png_data, &png_data_size, fn);   /*Load the file*/
         if(error) {
-            LV_LOG_WARN("error %u: %s", error, lodepng_error_text(error));
+            LV_LOG_WARN("error %" PRIu32 ": %s", error, lodepng_error_text(error));
             return LV_RES_INV;
         }
 
@@ -138,7 +138,7 @@ static lv_res_t decoder_open(lv_img_decoder_t * decoder, lv_img_decoder_dsc_t * 
         error = lodepng_decode32(&img_data, &png_width, &png_height, png_data, png_data_size);
         lv_mem_free(png_data); /*Free the loaded file*/
         if(error) {
-            LV_LOG_WARN("error %u: %s", error, lodepng_error_text(error));
+            LV_LOG_WARN("error %" PRIu32 ": %s", error, lodepng_error_text(error));
             return LV_RES_INV;
         }
 
