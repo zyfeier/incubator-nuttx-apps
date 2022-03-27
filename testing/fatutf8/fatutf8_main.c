d0c63d32a5ee25cfa363f9960342b22ed6b33efa
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-03-27 14:16:30 +0800

    testing/fatutf8: Sync with the mainline
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
    Change-Id: Ib2fb93960649393b4ef0d3bfee3285bf09dcf02f

--- testing/fatutf8/fatutf8_main.c
+++ testing/fatutf8/fatutf8_main.c
@@ -60,8 +60,7 @@ int main(int argc, FAR char *argv[])
   int len;
 
   char buf[128];
-  char path[256];
-  char dir_path[256];
+  char path[PATH_MAX];
 
   char *basepath = NULL;
 
@@ -109,7 +108,6 @@ int main(int argc, FAR char *argv[])
 
   printf("\n");
 
-  strcpy(dir_path, path);
   strcat(path, FILE_NAME);
 
   printf("open(%s)\n", path);
@@ -174,15 +172,20 @@ int main(int argc, FAR char *argv[])
   if (ret == 0)
     {
       printf("removed %s\n", path);
-      ret = remove(dir_path);
-      if (ret == 0)
+      basepath = strrchr(path, '/');
+      if (basepath != NULL)
         {
-          printf("removed %s\n", dir_path);
-        }
-      else
-        {
-          printf("remove failed: %d\n", errno);
-          exit(ret);
+          *basepath = '\0';
+          ret = remove(path);
+          if (ret == 0)
+            {
+              printf("removed %s\n", path);
+            }
+          else
+            {
+              printf("remove failed: %d\n", errno);
+              exit(ret);
+            }
         }
     }
   else
