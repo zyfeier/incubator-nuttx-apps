f9a2827cec40bb938b0a9d9d960bb359fc1d08e1
pengyiqiang <pengyiqiang@xiaomi.com>
2022-08-19 12:19:52 +0800

    lvgl/lv_porting: support for discontinuous double framebuffer
    VELAPLATFO-3602
    
    Change-Id: I960c757c07d64019d9a71614eeb9384741bf382d
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- graphics/lvgl/lv_porting/lv_fbdev_interface.c
+++ graphics/lvgl/lv_porting/lv_fbdev_interface.c
@@ -29,91 +29,19 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <fcntl.h>
-#include <debug.h>
 #include <errno.h>
-#include <pthread.h>
-#include <semaphore.h>
 #include <lv_porting/lv_porting.h>
 
 /****************************************************************************
  * Pre-processor Definitions
  ****************************************************************************/
 
-#if defined(CONFIG_ARM_HAVE_NEON) && LV_COLOR_DEPTH == 32
-extern void rotate_neon(void* dst, void* src, uint32_t stride, uint32_t wh);
-#endif
-
-#if defined(CONFIG_LV_FBDEV_USE_STATIC_BUFFER)
-
-#define LV_FBDEV_BUFFER_SIZE \
-       (CONFIG_LV_FBDEV_HOR_RES * CONFIG_LV_FBDEV_VER_RES)
-
-#if defined(CONFIG_LV_FBDEV_USE_CUSTOM_SECTION)
-#define LV_FBDEV_BUFFER_SECTION \
-        __attribute__((section(CONFIG_LV_FBDEV_SECTION_NAME)))
-#else
-#define LV_FBDEV_BUFFER_SECTION
-#endif
-
-#endif
-
 #if defined(CONFIG_FB_UPDATE)
 #define FBDEV_UPDATE_AREA(obj, area) fbdev_update_area(obj, area)
 #else
 #define FBDEV_UPDATE_AREA(obj, area)
 #endif
-
-#if  defined(CONFIG_LV_FBDEV_ROTATE_90)  \
-  || defined(CONFIG_LV_FBDEV_ROTATE_180) \
-  || defined(CONFIG_LV_FBDEV_ROTATE_270)
-#define FBDEV_ROTATE_ENABLE 1
-#else
-#define FBDEV_ROTATE_ENABLE 0
-#endif
-
-#define FB_GET_POS(x, y) (fbp + (y) * fb_xres + (x))
-
-#define REPEAT8(expr) expr expr expr expr expr expr expr expr
-
-#if defined(CONFIG_LV_FBDEV_ROTATE_90)
-
-#define FB_COPY             \
-  *cur_pos = *color_p++;    \
-  cur_pos += fb_xres;
-
-#elif defined(CONFIG_LV_FBDEV_ROTATE_180)
-
-#define FB_COPY             \
-  *cur_pos = *color_p++;    \
-  cur_pos--;
-
-#elif defined(CONFIG_LV_FBDEV_ROTATE_270)
-
-#define FB_COPY             \
-  *cur_pos = *color_p++;    \
-  cur_pos -= fb_xres;
-
-#endif
-
-#define FB_COPY_LINE(width) \
-  do {                      \
-    int len = width;        \
-    while (len > 32)        \
-      {                     \
-        REPEAT8(FB_COPY);   \
-        REPEAT8(FB_COPY);   \
-        REPEAT8(FB_COPY);   \
-        REPEAT8(FB_COPY);   \
-        len -= 32;          \
-      }                     \
-    while (len)             \
-      {                     \
-        FB_COPY             \
-        len--;              \
-      }                     \
-  } while(0)
 
 /****************************************************************************
  * Private Type Declarations
@@ -132,6 +60,7 @@ struct fbdev_obj_s
 
   int fd;
   FAR void *fbmem;
+  uint32_t fbmem2_yoffset;
   struct fb_videoinfo_s vinfo;
   struct fb_planeinfo_s pinfo;
 
@@ -151,39 +80,6 @@ struct fbdev_obj_s
  * Private Functions
  ****************************************************************************/
 
-#if FBDEV_ROTATE_ENABLE
-
-/****************************************************************************
- * Name: rotate_area
- ****************************************************************************/
-
-static void rotate_area(FAR lv_area_t *res,
-                        FAR const lv_area_t *ori,
-                        lv_coord_t hor_max,
-                        lv_coord_t ver_max)
-{
-#if defined(CONFIG_LV_FBDEV_ROTATE_90)
-    lv_coord_t h = lv_area_get_height(ori) - 1;
-    res->x1 = ver_max - ori->y1 - h;
-    res->y1 = ori->x1;
-    res->x2 = ver_max - ori->y2 + h;
-    res->y2 = ori->x2;
-#elif defined(CONFIG_LV_FBDEV_ROTATE_180)
-    res->x1 = hor_max - ori->x2;
-    res->y1 = ver_max - ori->y2;
-    res->x2 = hor_max - ori->x1;
-    res->y2 = ver_max - ori->y1;
-#elif defined(CONFIG_LV_FBDEV_ROTATE_270)
-    lv_coord_t w = lv_area_get_width(ori) - 1;
-    res->x1 = ori->y1;
-    res->y1 = hor_max - ori->x1 - w;
-    res->x2 = ori->y2;
-    res->y2 = hor_max - ori->x2 + w;
-#endif
-}
-
-#endif /* FBDEV_ROTATE_ENABLE */
-
 /****************************************************************************
  * Name: buf_rotate_copy
  ****************************************************************************/
@@ -193,15 +89,6 @@ static void fbdev_update_area(FAR struct fbdev_obj_s *fbdev_obj,
                               FAR const lv_area_t *area_p)
 {
   struct fb_area_s fb_area;
-
-#if FBDEV_ROTATE_ENABLE
-  lv_area_t area_rot;
-  rotate_area(&area_rot,
-              area_p,
-              fbdev_obj->disp_drv.hor_res - 1,
-              fbdev_obj->disp_drv.ver_res - 1);
-  area_p = &area_rot;
-#endif
 
   fb_area.x = area_p->x1;
   fb_area.y = area_p->y1;
@@ -298,11 +185,11 @@ static void fbdev_switch_buffer(FAR struct fbdev_obj_s *fbdev_obj)
     {
       fbdev_obj->pinfo.yoffset = 0;
       fbdev_obj->act_buffer = fbdev_obj->fbmem
-          + fbdev_obj->vinfo.yres * fbdev_obj->pinfo.stride;
+        + fbdev_obj->fbmem2_yoffset * fbdev_obj->pinfo.stride;
     }
   else
     {
-      fbdev_obj->pinfo.yoffset = fbdev_obj->vinfo.yres;
+      fbdev_obj->pinfo.yoffset = fbdev_obj->fbmem2_yoffset;
       fbdev_obj->act_buffer = fbdev_obj->fbmem;
     }
 
@@ -314,47 +201,32 @@ static void fbdev_switch_buffer(FAR struct fbdev_obj_s *fbdev_obj)
   LV_LOG_TRACE("finished");
 }
 
-#ifdef CONFIG_FB_SYNC
+#if defined(CONFIG_FB_SYNC)
 
 /****************************************************************************
- * Name: fbdev_check_sync
+ * Name: fbdev_disp_vsync_refr
  ****************************************************************************/
 
-static bool fbdev_check_sync(FAR struct fbdev_obj_s *fbdev_obj)
+static void fbdev_disp_vsync_refr(FAR lv_timer_t *timer)
 {
   int ret;
-  LV_LOG_TRACE("Check sync");
+  FAR struct fbdev_obj_s *fbdev_obj = timer->user_data;
+
+  LV_LOG_TRACE("Check vsync...");
 
   ret = ioctl(fbdev_obj->fd, FBIO_WAITFORVSYNC, NULL);
   if (ret != OK)
     {
-      LV_LOG_TRACE("No sync signal detect");
-      return false;
-    }
-
-  LV_LOG_TRACE("Sync ok!, disp refr start");
-  return true;
-}
-
-#endif /* CONFIG_FB_SYNC */
-
-/****************************************************************************
- * Name: fbdev_disp_refr
- ****************************************************************************/
-
-static void fbdev_disp_refr(FAR lv_timer_t *timer)
-{
-#ifdef CONFIG_FB_SYNC
-  FAR struct fbdev_obj_s *fbdev_obj = timer->user_data;
-
-  if (!fbdev_check_sync(fbdev_obj))
-    {
+      LV_LOG_TRACE("No vsync signal detect");
       return;
     }
-#endif
+
+  LV_LOG_TRACE("Refresh start");
 
   _lv_disp_refr_timer(NULL);
 }
+
+#endif /* CONFIG_FB_SYNC */
 
 /****************************************************************************
  * Name: fbdev_render_start
@@ -397,20 +269,6 @@ static void fbdev_render_start(FAR lv_disp_drv_t *disp_drv)
         }
     }
 
-#if FBDEV_ROTATE_ENABLE
-  /* Rotate inv_areas for buffer synchronization */
-
-  for (i = 0; i < fbdev_obj->inv_areas_len; i++)
-    {
-      lv_area_t area_rot;
-      rotate_area(&area_rot,
-                  &fbdev_obj->inv_areas[i],
-                  hor_res - 1,
-                  ver_res - 1);
-      fbdev_obj->inv_areas[i] = area_rot;
-    }
-#endif
-
   /* Sync the dirty area of ​​the previous frame */
 
   fbdev_copy_areas(fbdev_obj->act_buffer, fbdev_obj->last_buffer,
@@ -419,8 +277,6 @@ static void fbdev_render_start(FAR lv_disp_drv_t *disp_drv)
 
   fbdev_obj->inv_areas_len = 0;
 }
-
-#if defined(CONFIG_LV_FBDEV_USE_DOUBLE_BUFFER)
 
 /****************************************************************************
  * Name: fbdev_flush_direct
@@ -447,37 +303,6 @@ static void fbdev_flush_direct(FAR lv_disp_drv_t *disp_drv,
   /* Tell the flushing is ready */
 
   lv_disp_flush_ready(disp_drv);
-}
-
-#else
-
-/****************************************************************************
- * Name: fbdev_get_buffer
- ****************************************************************************/
-
-static FAR lv_color_t *fbdev_get_buffer(int hor_res, int ver_res)
-{
-  FAR lv_color_t *retval;
-#if defined(CONFIG_LV_FBDEV_USE_STATIC_BUFFER)
-  static lv_color_t buffer[LV_FBDEV_BUFFER_SIZE] LV_FBDEV_BUFFER_SECTION;
-
-  /* Check if buffer size match */
-
-  if (hor_res != CONFIG_LV_FBDEV_HOR_RES
-      || ver_res != CONFIG_LV_FBDEV_VER_RES)
-    {
-      LV_LOG_ERROR("Resolution mismatch, fb: %dx%d, lvgl: %dx%d.",
-                   hor_res, ver_res,
-                   CONFIG_LV_FBDEV_HOR_RES, CONFIG_LV_FBDEV_VER_RES);
-      return NULL;
-    }
-
-  retval = buffer;
-#else
-  retval = malloc(hor_res * ver_res * sizeof(lv_color_t));
-#endif
-  LV_LOG_TRACE("%p", retval);
-  return retval;
 }
 
 /****************************************************************************
@@ -539,75 +364,17 @@ static void fbdev_flush_normal(FAR lv_disp_drv_t *disp_drv,
 
   FAR lv_color_t *fbp = fbdev_obj->act_buffer;
   fb_coord_t fb_xres = fbdev_obj->vinfo.xres;
-  FAR lv_color_t *cur_pos;
-
-#if FBDEV_ROTATE_ENABLE
-  lv_coord_t hor_max = disp_drv->hor_res - 1;
-  lv_coord_t ver_max = disp_drv->ver_res - 1;
-#endif
+  int hor_size = w * sizeof(lv_color_t);
+  FAR lv_color_t *cur_pos = fbp + y1 * fb_xres + x1;
 
   LV_LOG_TRACE("start copy");
 
-#if !FBDEV_ROTATE_ENABLE
-  int hor_size = w * sizeof(lv_color_t);
-  cur_pos = FB_GET_POS(x1, y1);
   for (y = y1; y <= y2; y++)
     {
       lv_memcpy(cur_pos, color_p, hor_size);
       cur_pos += fb_xres;
       color_p += w;
     }
-#elif defined(CONFIG_LV_FBDEV_ROTATE_90)
-  LV_UNUSED(hor_max);
-#if defined(CONFIG_ARM_HAVE_NEON) && LV_COLOR_DEPTH == 32
-  y = y2 - y1 + 1;
-  uint32_t wh = w | y << 16;
-  cur_pos = FB_GET_POS(ver_max - y2, x1);
-  if (w > 3 && y > 3) {
-    rotate_neon(cur_pos, color_p, fb_xres * sizeof(lv_color_t), wh);
-  }
-  else
-#endif
-  for (y = y1; y <= y2; y++)
-    {
-      /* x' = ver_max - y
-       * y' = x
-       */
-
-      cur_pos = FB_GET_POS(ver_max - y, x1);
-      FB_COPY_LINE(w);
-    }
-#elif defined(CONFIG_LV_FBDEV_ROTATE_180)
-  for (y = y1; y <= y2; y++)
-    {
-      /* x' = hor_max - x
-       * y' = ver_max - y
-       */
-
-      cur_pos = FB_GET_POS(hor_max - x1, ver_max - y);
-      FB_COPY_LINE(w);
-    }
-#elif defined(CONFIG_LV_FBDEV_ROTATE_270)
-  LV_UNUSED(ver_max);
-#if defined(CONFIG_ARM_HAVE_NEON) && LV_COLOR_DEPTH == 32
-  y = y2 - y1 + 1;
-  uint32_t wh = w | y << 16;
-  cur_pos = FB_GET_POS(y1, hor_max - area_p->x2);
-  if (w > 3 && y > 3) {
-    rotate_neon(cur_pos, color_p, fb_xres * sizeof(lv_color_t), wh);
-  }
-  else
-#endif
-  for (y = y1; y <= y2; y++)
-    {
-      /* x' = y
-       * y' = hor_max - x
-       */
-
-      cur_pos = FB_GET_POS(y, hor_max - x1);
-      FB_COPY_LINE(w);
-    }
-#endif
 
   LV_LOG_TRACE("end copy");
 
@@ -632,6 +399,8 @@ static void fbdev_flush_convert(FAR lv_disp_drv_t *disp_drv,
   const fb_coord_t xres = fbdev_obj->vinfo.xres;
   int x;
   int y;
+
+  LV_LOG_TRACE("start copy");
 
   switch (bpp)
     {
@@ -685,10 +454,100 @@ static void fbdev_flush_convert(FAR lv_disp_drv_t *disp_drv,
         break;
     }
 
+  LV_LOG_TRACE("end copy");
+
   fbdev_update_part(fbdev_obj, disp_drv, area_p);
 }
 
-#endif /* CONFIG_LV_FBDEV_USE_DOUBLE_BUFFER */
+/****************************************************************************
+ * Name: fbdev_get_pinfo
+ ****************************************************************************/
+
+static int fbdev_get_pinfo(int fd, FAR struct fb_planeinfo_s *pinfo)
+{
+  int ret = ioctl(fd, FBIOGET_PLANEINFO,
+                  (unsigned long)((uintptr_t)pinfo));
+  if (ret < 0)
+    {
+      LV_LOG_ERROR("ERROR: ioctl(FBIOGET_PLANEINFO) failed: %d", errno);
+      return ret;
+    }
+
+  LV_LOG_INFO("PlaneInfo (plane %d):", pinfo->display);
+  LV_LOG_INFO("    fbmem: %p", pinfo->fbmem);
+  LV_LOG_INFO("    fblen: %lu", (unsigned long)pinfo->fblen);
+  LV_LOG_INFO("   stride: %u", pinfo->stride);
+  LV_LOG_INFO("  display: %u", pinfo->display);
+  LV_LOG_INFO("      bpp: %u", pinfo->bpp);
+
+  /* Only these pixel depths are supported.  viinfo.fmt is ignored, only
+   * certain color formats are supported.
+   */
+
+  if (pinfo->bpp != 32 && pinfo->bpp != 16 &&
+      pinfo->bpp != 8  && pinfo->bpp != 1)
+    {
+      LV_LOG_ERROR("bpp = %u not supported", pinfo->bpp);
+      return -1;
+    }
+
+  return 0;
+}
+
+/****************************************************************************
+ * Name: fbdev_try_init_fbmem2
+ ****************************************************************************/
+
+static int fbdev_try_init_fbmem2(FAR struct fbdev_obj_s *state)
+{
+  uint32_t buf_offset;
+  struct fb_planeinfo_s pinfo;
+
+  memset(&pinfo, 0, sizeof(pinfo));
+
+  /* Get display[1] planeinfo */
+
+  pinfo.display = state->pinfo.display + 1;
+
+  if (fbdev_get_pinfo(state->fd, &pinfo) < 0)
+    {
+      return -1;
+    }
+
+  /* check displey and match bpp */
+
+  if (!(pinfo.display != state->pinfo.display
+     && pinfo.bpp == state->pinfo.bpp))
+    {
+      LV_LOG_WARN("fbmem2 is incorrect");
+      return -1;
+    }
+
+  /* Check the buffer address offset,
+   * It needs to be divisible by pinfo.stride
+   */
+
+  buf_offset = pinfo.fbmem - state->fbmem;
+
+  if ((buf_offset % state->pinfo.stride) != 0)
+    {
+      LV_LOG_ERROR("The buf_offset(%" PRIu32 ") is incorrect,"
+                   " It needs to be divisible"
+                   " by pinfo.stride(%d)",
+                   buf_offset, state->pinfo.stride);
+      return -1;
+    }
+
+  /* Enable double buffer mode */
+
+  state->double_buffer = true;
+  state->fbmem2_yoffset = buf_offset / state->pinfo.stride;
+
+  LV_LOG_INFO("Use non-consecutive fbmem2 = %p, yoffset = %" PRIu32,
+              pinfo.fbmem, state->fbmem2_yoffset);
+
+  return 0;
+}
 
 /****************************************************************************
  * Name: fbdev_init
@@ -698,9 +557,11 @@ static FAR lv_disp_t *fbdev_init(FAR struct fbdev_obj_s *state)
 {
   FAR struct fbdev_obj_s *fbdev_obj = malloc(sizeof(struct fbdev_obj_s));
   FAR lv_disp_drv_t *disp_drv;
-  FAR lv_color_t *buf;
   int fb_xres = state->vinfo.xres;
   int fb_yres = state->vinfo.yres;
+  size_t fb_size = fb_xres * fb_yres;
+  FAR lv_color_t *buf1 = NULL;
+  FAR lv_color_t *buf2 = NULL;
 
   if (fbdev_obj == NULL)
     {
@@ -715,81 +576,59 @@ static FAR lv_disp_t *fbdev_init(FAR struct fbdev_obj_s *state)
   disp_drv->draw_buf = &(fbdev_obj->disp_draw_buf);
   disp_drv->screen_transp = false;
   disp_drv->user_data = fbdev_obj;
+  disp_drv->hor_res = fb_xres;
+  disp_drv->ver_res = fb_yres;
 
 #if defined(CONFIG_LV_USE_GPU_INTERFACE)
   disp_drv->draw_ctx_init = lv_gpu_draw_ctx_init;
   disp_drv->draw_ctx_size = sizeof(gpu_draw_ctx_t);
 #endif /* CONFIG_LV_USE_GPU_INTERFACE */
 
-#if defined(CONFIG_LV_FBDEV_USE_DOUBLE_BUFFER)
-  if (!fbdev_obj->double_buffer)
+  if (fbdev_obj->double_buffer && fbdev_obj->color_match)
     {
-      LV_LOG_ERROR("fbdev does not support double buffering");
-      goto failed;
-    }
+      LV_LOG_INFO("Double buffer mode");
 
-  if (!fbdev_obj->color_match)
-    {
-      LV_LOG_ERROR("fbdev and lvgl color depth do not match");
-      goto failed;
-    }
+      buf1 = fbdev_obj->fbmem;
+      buf2 = fbdev_obj->fbmem
+        + fbdev_obj->fbmem2_yoffset * fbdev_obj->pinfo.stride;
 
-  buf = fbdev_obj->fbmem;
-
-  lv_disp_draw_buf_init(&(fbdev_obj->disp_draw_buf),
-                        buf, buf + fb_xres * fb_yres,
-                        fb_xres * fb_yres);
-
-  disp_drv->direct_mode = true;
-  disp_drv->flush_cb = fbdev_flush_direct;
-  disp_drv->hor_res = fb_xres;
-  disp_drv->ver_res = fb_yres;
-#else
-  buf = fbdev_get_buffer(fb_xres, fb_yres);
-
-  if (!buf)
-    {
-      LV_LOG_ERROR("Unable to get buffer");
-      goto failed;
-    }
-
-  lv_disp_draw_buf_init(&(fbdev_obj->disp_draw_buf), buf, NULL,
-                        fb_xres * fb_yres);
-
-  disp_drv->flush_cb = fbdev_obj->color_match
-                        ? fbdev_flush_normal
-                        : fbdev_flush_convert;
-
-#if defined(CONFIG_LV_FBDEV_ROTATE_90) || defined(CONFIG_LV_FBDEV_ROTATE_270)
-  disp_drv->hor_res = fb_yres;
-  disp_drv->ver_res = fb_xres;
-#else
-  disp_drv->hor_res = fb_xres;
-  disp_drv->ver_res = fb_yres;
-#endif
-
-#endif /* CONFIG_LV_FBDEV_USE_DOUBLE_BUFFER */
-
-  if (fbdev_obj->double_buffer)
-    {
+      disp_drv->direct_mode = true;
+      disp_drv->flush_cb = fbdev_flush_direct;
       disp_drv->render_start_cb = fbdev_render_start;
     }
+  else
+    {
+      LV_LOG_INFO("Single buffer mode");
 
+      buf1 = malloc(fb_size * sizeof(lv_color_t));
+      LV_ASSERT_MALLOC(buf1);
+
+      if (!buf1)
+        {
+          LV_LOG_ERROR("failed to malloc draw buffer");
+          goto failed;
+        }
+
+      disp_drv->flush_cb = fbdev_obj->color_match
+                            ? fbdev_flush_normal
+                            : fbdev_flush_convert;
+    }
+
+  lv_disp_draw_buf_init(&(fbdev_obj->disp_draw_buf), buf1, buf2, fb_size);
   fbdev_obj->act_buffer = fbdev_obj->fbmem;
   fbdev_obj->disp = lv_disp_drv_register(&(fbdev_obj->disp_drv));
 
-  /* If double buffer is supported, use active refresh method */
+#if defined(CONFIG_FB_SYNC)
+  /* If double buffer and vsync is supported, use active refresh method */
 
-  if (fbdev_obj->double_buffer)
+  if (fbdev_obj->disp_drv.direct_mode)
     {
       FAR lv_timer_t *refr_timer = _lv_disp_get_refr_timer(fbdev_obj->disp);
       lv_timer_del(refr_timer);
       fbdev_obj->disp->refr_timer = NULL;
-      lv_timer_create(fbdev_disp_refr,
-                      LV_DISP_DEF_REFR_PERIOD / 4,
-                      fbdev_obj);
-      LV_LOG_INFO("Enable double framebuffer mode");
+      lv_timer_create(fbdev_disp_vsync_refr, 1, fbdev_obj);
     }
+#endif
 
   return fbdev_obj->disp;
 
@@ -857,30 +696,10 @@ FAR lv_disp_t *lv_fbdev_interface_init(FAR const char *dev_path,
   LV_LOG_INFO("     yres: %u", state.vinfo.yres);
   LV_LOG_INFO("  nplanes: %u", state.vinfo.nplanes);
 
-  ret = ioctl(state.fd, FBIOGET_PLANEINFO,
-              (unsigned long)((uintptr_t)&state.pinfo));
+  ret = fbdev_get_pinfo(state.fd, &state.pinfo);
+
   if (ret < 0)
     {
-      LV_LOG_ERROR("ERROR: ioctl(FBIOGET_PLANEINFO) failed: %d", errno);
-      close(state.fd);
-      return NULL;
-    }
-
-  LV_LOG_INFO("PlaneInfo (plane 0):");
-  LV_LOG_INFO("    fbmem: %p", state.pinfo.fbmem);
-  LV_LOG_INFO("    fblen: %lu", (unsigned long)state.pinfo.fblen);
-  LV_LOG_INFO("   stride: %u", state.pinfo.stride);
-  LV_LOG_INFO("  display: %u", state.pinfo.display);
-  LV_LOG_INFO("      bpp: %u", state.pinfo.bpp);
-
-  /* Only these pixel depths are supported.  viinfo.fmt is ignored, only
-   * certain color formats are supported.
-   */
-
-  if (state.pinfo.bpp != 32 && state.pinfo.bpp != 16 &&
-      state.pinfo.bpp != 8  && state.pinfo.bpp != 1)
-    {
-      LV_LOG_ERROR("bpp = %u not supported", state.pinfo.bpp);
       close(state.fd);
       return NULL;
     }
@@ -920,12 +739,22 @@ FAR lv_disp_t *lv_fbdev_interface_init(FAR const char *dev_path,
 
   LV_LOG_INFO("Mapped FB: %p", state.fbmem);
 
+  if (state.double_buffer)
+    {
+      state.fbmem2_yoffset = state.vinfo.yres;
+    }
+  else
+    {
+      fbdev_try_init_fbmem2(&state);
+    }
+
   disp = fbdev_init(&state);
 
-  if (disp == NULL)
+  if (!disp)
     {
       munmap(state.fbmem, state.pinfo.fblen);
       close(state.fd);
+      return NULL;
     }
 
   return disp;
