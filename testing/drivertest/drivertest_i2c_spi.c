e29fb892fe6aff012c57eb95be70ffc68c2b1c03
xinbingnan <xinbingnan@xiaomi.com>
2023-01-11 14:07:57 +0800

    drivertest_i2c_spi.c: fix path in header comment
    VELAPLATFO-5145
    
    Signed-off-by: xinbingnan <xinbingnan@xiaomi.com>
    Change-Id: I3b81cdf09e24c744ac61604d8edc599a46a56d12

--- testing/drivertest/drivertest_i2c_spi.c
+++ testing/drivertest/drivertest_i2c_spi.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * apps/testing/drivertest/drivertest_i2c.c
+ * apps/testing/drivertest/drivertest_i2c_spi.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
