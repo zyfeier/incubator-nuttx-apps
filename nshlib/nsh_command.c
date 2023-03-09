c16981807d1ba721eddc90ab475a9d2dee7248db
yinshengkai <yinshengkai@xiaomi.com>
2023-03-09 12:46:53 +0800

    nsh: fix a buffer overflow in help
    VELAPLATFO-6182
    
    Signed-off-by: yinshengkai <yinshengkai@xiaomi.com>
    Change-Id: Ide339791afcd19b9476ab3fe4b7eb92020fc02a7

--- nshlib/nsh_command.c
+++ nshlib/nsh_command.c
@@ -618,9 +618,7 @@ static inline void help_cmdlist(FAR struct nsh_vtbl_s *vtbl)
   unsigned int k;
   unsigned int offset;
 
-  /* Extra 5 bytes for tab before newline and '\0' */
-
-  char line[HELP_LINELEN + HELP_TABSIZE + 1];
+  char line[HELP_LINELEN];
 
   /* Pick an optimal column width */
 
@@ -633,7 +631,7 @@ static inline void help_cmdlist(FAR struct nsh_vtbl_s *vtbl)
         }
     }
 
-  colwidth += 2;
+  colwidth += HELP_TABSIZE;
 
   /* Determine the number of commands to put on one line */
 
@@ -844,7 +842,7 @@ static inline void help_builtins(FAR struct nsh_vtbl_s *vtbl)
       return;
     }
 
-  column_width += 2;
+  column_width += HELP_TABSIZE;
 
   /* Determine the number of commands to put on one line */
 
@@ -867,7 +865,7 @@ static inline void help_builtins(FAR struct nsh_vtbl_s *vtbl)
   nsh_write(vtbl, g_builtin_prompt, strlen(g_builtin_prompt));
   for (i = 0; i < num_builtin_rows; i++)
     {
-      offset = 4;
+      offset = HELP_TABSIZE;
       memset(line, ' ', offset);
 
       for (j = 0, k = i;
